50d304e64c0768d8f3082b37e786bc4987289815
John Jacquay <john@bioteam.net>
2019-11-17 06:52:59 -0700

    remove secrets from env vars

--- env.sh
+++ env.sh
@@ -6,8 +6,6 @@ export AWS_SECRET_REGION=us-east-1
 
 export IRODS_HOST=<your-host> \ # irods.johnjacquay.com
 IRODS_PORT=<your-port> \        # 1247
-IRODS_USER=<your-user> \        # rods
-IRODS_PASSWORD=<your-pass> \    # testpassword
 IRODS_ZONE=<your-zone> \        # tempZone
 IRODS_S3_RESC=<your-resc> \     # s3resc
 IRODS_VAULT_PREFIX=<your-vault>  # irods/Vault
